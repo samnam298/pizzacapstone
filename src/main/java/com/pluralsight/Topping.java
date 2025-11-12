a2ae5209f62d6bafae1a14b9776573db958d26d6
samnam298 <samrawithailena@gmail.com>
2025-11-12 15:11:37 -0500

    Create abstract Item class with name field and Add getters/setters and abstract getPrice() method

---
+++ src/main/java/com/pluralsight/Topping.java
@@ -0,0 +1,10 @@
+public abstract class Topping {
+    private String name;
+
+    public Topping(String name) { this.name = name; }
+
+    public String getName() { return name; }
+
+    public abstract double getPrice(String size);
+}
+
